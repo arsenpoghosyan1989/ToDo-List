771d0711b9e7f9a8c80592b8c05679b76b989414
Arsen Poghosyan <arsenpoghosyan1989@gmail.com>
2020-08-07 19:32:42 +0400

    Updates

---
+++ precache-manifest.bc86b1eccd5b3c866a87ebd28745f6f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1aad5574c6a00607e57e31e21c34bd59",
+    "url": "/ToDo-List/index.html"
+  },
+  {
+    "revision": "a4287757fbf669c11849",
+    "url": "/ToDo-List/static/css/main.b6443511.chunk.css"
+  },
+  {
+    "revision": "3cc0c57a2b7735115827",
+    "url": "/ToDo-List/static/js/2.30f47b4f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDo-List/static/js/2.30f47b4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4287757fbf669c11849",
+    "url": "/ToDo-List/static/js/main.2a83a574.chunk.js"
+  },
+  {
+    "revision": "ba30ece2b971d1f08bfb",
+    "url": "/ToDo-List/static/js/runtime-main.47196ccb.js"
+  }
+]);
